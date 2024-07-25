07e134ce7ca525c48f5aab5cd1857d4674d41340
Anne-Zwift <145778485+Anne-Zwift@users.noreply.github.com>
2024-07-25 11:22:30 +0200

    adding contact.js form

---
+++ js/contact.js
@@ -0,0 +1,34 @@
+document.forms.contact.addEventListener('submit', (event) => {
+    //Prevent the form from refreshing
+    event.preventDefault();
+
+    //Get the form element
+    const form = event.target;
+
+    //Create a new FormData object
+    const formData = new FormData(form);
+
+    //Convert the FormData object to a JSON object
+    const json = Object.fromEntries(formData.entries());
+
+    //Log the JSON object to the console
+    //alert(JSON.stringify(json, null, 2));
+    alert(JSON.stringify('Thank you! Your message was successfully sent.'));
+});
+
+/*document.forms.contact.addEventListener('submit', (event) => {
+    // Prevent the form from refreshing
+    event.preventDefault();
+  
+    // Get the form element
+    const form = event.target;
+  
+    // Create a new FormData object
+    const formData = new FormData(form);
+  
+    // Convert the FormData object to a JSON object
+    const json = Object.fromEntries(formData.entries());
+  
+    // Log the JSON object to the console
+    alert(JSON.stringify(json, null, 2));
+  });*/
