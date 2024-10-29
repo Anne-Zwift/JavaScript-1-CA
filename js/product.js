fefa694bd48973d0194c6e6475bff1935e9f766b
Anne-Zwift <145778485+Anne-Zwift@users.noreply.github.com>
2024-10-29 16:24:53 +0100

    adding pages and fixing functionality add items to cart on every pages

--- js/product.js
+++ js/product.js
@@ -2,9 +2,14 @@
 const spinner = document.querySelector("#loading");
 
 //display data: set up HTML skeleton for displaying data
-const singleProductsContainer = document.querySelector("#single-products-container");
+const singleProductsContainer = document.querySelector(".single-products-container");
 
-
+const cartCount = document.querySelector("#cartCount");
+//initialize the cart array
+const storedCartItems = JSON.parse(localStorage.getItem("cart"));
+//this gets the cart items in local storage and keeps it in the cart
+let cart = storedCartItems ? storedCartItems: [];
+cartCount.textContent = cart.length;
 
 //fetching the API single product link
 const API_Single_Product = "https://v2.api.noroff.dev/rainy-days/b8b528fc-6c60-41f6-a5a9-9a8b27a9482a";
@@ -14,28 +19,45 @@ async function fetchDataSingleProduct() {
 
     try {
         
-       //spinner - see if it works in 3G
-        spinner.style.display = 'inline-block';
-         //fetching the data
-        //const productId = new URLSearchParams(window.location.search).get("id");//fetching the web browser id not in use
-        const response = await fetch(API_Single_Product);//(specifics: I need to use id after rainy-days, I can then fetch every viewed jacket
+     //spinner - see if it works in 3G
+      spinner.style.display = 'inline-block';
+    //fetching the data
+      const response = await fetch(API_Single_Product);
 
-        const data = await response.json();//convert it back to js and save it in to data
+      const data = await response.json();//convert it back to js and save it in to data
 
 
-        if (!response.ok) {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
 
-        };
+      //display data
+      displayData(data.data);
 
-        //display data
-        displayData(data.data);
-
+      //Add to cart functionality
+      //select cart Button
+      const addToCartBtns = document.querySelectorAll(".add-to-cart");
         
-        
-        
+      //loop thru the btns
+      addToCartBtns.forEach((btn) => {
+        btn.addEventListener('click', (event) => {
+          const productId = event.target.dataset.id;
+          const product = data;//since selecting a single product
 
+          cart.push(product);
+          //update the cart count
+          cartCount.textContent = cart.length;
+          //add items to the local storage
+          localStorage.setItem("cart", JSON.stringify(cart));
+          //show notification
+          showNotification("Product Added to Cart");
+      });
+  });
+      
     } catch (error) {
-
+      console.error('Error fetching single product:', error);
+    } finally {
+      spinner.style.display = 'none';//hide spinner
     }
 }
 fetchDataSingleProduct();
@@ -43,29 +65,26 @@ fetchDataSingleProduct();
 //create a function for a single product
 
 function displayData(data) {
-  
+  const { image, title, description, price, sizes, gender, id } = data;
   singleProductsContainer.innerHTML = `
-    
-  <div class="single-products-container" id="single-products-container">
-    <div id="loading"></div>
-      <div class="product">
-      <div class="product-image">
-      <img src="${data.image.url}" alt="">
-      </div>
-      <p class="product-name">${data.title}</p>
-      <div class="detail">
-        <div><p class="description">${data.description}</p></div><br>
-        <div><p class="price">${data.price}</p></div><br>
-        <p class="sizes">${data.sizes.join(" ")}</p>
-        <div><p class="gender">${data.gender}</p></div>
-      </div>
+  <div class="product">
+    <div class="product-image">
+    <img src="${image.url}" alt="">
+    </div>
+    <p class="product-name">${title}</p>
+    <div class="detail">
+    <div><p class="description">${description}</p></div><br>
+    <div><p class="price">${price}</p></div><br>
+    <p class="sizes">${sizes.join(" ")}</p>
+    <div><p class="gender">${gender}</p></div>
+    <button data-id=${id} class="add-to-cart">Add to Cart</button>
+    </div>
     </div>
   </div>
    
         `;
-
-    spinner.style.display = 'none';
-   
-
+    
 }
+
+
 
--- js/termsAndConditions.js
+++ js/termsAndConditions.js
@@ -1,22 +1,20 @@
 //fetching text to terms and conditions
-const textContainer = document.querySelector(".text-container");
-console.log(textContainer)
-
-
-const API_link = 'https://jsonplaceholder.typicode.com/todos/1';
+const API_link = 'https://developers.google.com/terms/';
 //console.log(termsAndConditions);//providing the link
+const termsContent = document.getElementById("terms-content");
+
 
 async function fetchData() {
   
   try {
     const response = await fetch(API_link);
 
-    if (response.ok) {
-      console.log('Network response was ok');//network error
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+      
     }
     
     const data = await response.json();
-    console.log(data);
     displayData([data]);//call displayData with fetched data wrapped in array
     
 
@@ -32,13 +30,10 @@ function displayData(data) {
   data.forEach((item) => {
     const dataTemplate = `
     
-    <div id="text-container" class="text-container">
-      <h3 class="terms-heading">Terms and Conditions</h3>
-      <p class="paragraph1">${item.userId}</p>
-      <p class="paragraph2">${item.title}</p>
-      <p class="paragraph3">${item.id}</p>
-      <button>Continue Shopping</button>
-    </div>
+      <div id="text-container" class="text-container">
+    <!--<h3 class="terms-heading">Terms and Conditions</h3>
+       <div id="terms-content"></div>
+      </div>
     
     
     `;
